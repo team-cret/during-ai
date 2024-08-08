42a5766239a959bfd7ed096215d853e92cbe774a
gunwoo99 <62701141+gunwoo99@users.noreply.github.com>
2024-08-09 01:45:48 +0900

    Fix : error, gemini project, config

--- ai_model/generation/gemini.py
+++ ai_model/generation/gemini.py
@@ -10,7 +10,7 @@ class GeminiTextGenerator:
     def generate_text(self, user_prompt:str, history:list[str], system_prompt:str) -> str:
         return self._generative_model.generate_content([system_prompt] + user_prompt).text
     
-    def generate_text_by_chat_bot(self, user_prompt:str, history:list[str], system_prompt:str) -> str:
+    def generate_text_by_chat_bot(self, user_prompt:str, history:list[str], system_prompt:str, is_stream:bool = False) -> str:
         history = [{
             'role' : 'user',
             'text' : system_prompt,
@@ -29,4 +29,4 @@ class GeminiTextGenerator:
                 })
 
         self.chat = self._generative_model.start_chat(history=processed_history)
-        return self.chat.send_message(user_prompt).text다음
+        return self.chat.send_message(user_prompt, stream=is_stream).text
--- setting/config.py
+++ setting/config.py
@@ -1,6 +1,7 @@
 from enum import Enum
 
 class Config(Enum):
+    GOOGLE_PROJECT_ID = 402205469404
     GOOGLE_API_KEY = 'GOOGLE_API_KEY'
     OPENAI_API_KEY = 'OPENAI_API_KEY'
     UPSTAGE_API_KEY = 'UPSTAGE_API_KEY'
