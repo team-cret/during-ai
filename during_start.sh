c9ffe9ba75e02366452e21291d643486378387aa
gunwoo99 <62701141+gunwoo99@users.noreply.github.com>
2024-11-13 15:35:31 +0900

    Fix : server number

--- during_start.sh
+++ during_start.sh
@@ -2,4 +2,4 @@
 cd ..
 . during/bin/activate
 cd during-ai
-gunicorn -k uvicorn.workers.UvicornWorker -w 4 --bind 0.0.0.0:3142 main:app
+gunicorn -k uvicorn.workers.UvicornWorker -w 1 --bind 0.0.0.0:3142 main:app
